0766a150ce22c72228d8f18310984608dfcdd1f9
Benoit Casseau <benoit@oxfordrobotics.institute>
2023-04-14 11:08:10 +0100

    remove unused cpp files

--- src/app/ddROSTfListener.cpp
+++
@@ -1,4 +0,0 @@
-#include "ddROSTfListener.h"
-
-std::shared_ptr<tf2_ros::TransformListener> ddROSTfListener::mTfListener = nullptr;
-std::shared_ptr<tf2_ros::Buffer> ddROSTfListener::mTfBuffer = nullptr;
